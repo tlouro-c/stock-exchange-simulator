e8c0fa1db8f89c3754251a292dd3868c4370f7b8
tlouro-c <tomascorreia.lc@gmail.com>
2024-10-14 12:21:55 +0100

    first commit

---
+++ broker/src/main/java/tc/tlouro_c/stock_exchange_simulator/Broker.java
@@ -0,0 +1,7 @@
+package tc.tlouro_c.stock_exchange_simulator;
+
+public class Broker {
+    public static void main(String[] args) {
+        System.out.println("Hello from broker!");
+    }
+}
---
+++ market/src/main/java/tc/tlouro_c/stock_exchange_simulator/Market.java
@@ -0,0 +1,7 @@
+package tc.tlouro_c.stock_exchange_simulator;
+
+public class Market {
+    public static void main(String[] args) {
+        System.out.println("Hello from market!");
+    }
+}
